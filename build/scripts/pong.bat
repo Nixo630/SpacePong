7ee638c08bbc836f55c983aa8091be4dfee72823
paris <paris.albin23@gmail.com>
2023-01-01 22:56:54 +0100

    Réglage d'un problème d'affichage

--- build/scripts/pong.bat
+++ build/scripts/pong.bat
@@ -67,11 +67,11 @@ goto fail
 :execute
 @rem Setup the command line
 
-set CLASSPATH=%APP_HOME%\lib\pong-1.0-SNAPSHOT.jar;%APP_HOME%\lib\javafx-controls-17-win.jar;%APP_HOME%\lib\javafx-graphics-17-win.jar;%APP_HOME%\lib\javafx-graphics-17.jar;%APP_HOME%\lib\javafx-base-17-win.jar;%APP_HOME%\lib\javafx-base-17.jar
+set CLASSPATH=%APP_HOME%\lib\SpacePong-1.0-SNAPSHOT.jar;%APP_HOME%\lib\javafx-fxml-17-win.jar;%APP_HOME%\lib\javafx-controls-17-win.jar;%APP_HOME%\lib\javafx-controls-17.jar;%APP_HOME%\lib\javafx-media-17-win.jar;%APP_HOME%\lib\javafx-graphics-17-win.jar;%APP_HOME%\lib\javafx-graphics-17.jar;%APP_HOME%\lib\javafx-base-17-win.jar;%APP_HOME%\lib\javafx-base-17.jar
 
 
 @rem Execute pong
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %PONG_OPTS%  -classpath "%CLASSPATH%" gui.App %*
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %PONG_OPTS%  -classpath "%CLASSPATH%" gui.Launcher %*
 
 :end
 @rem End local scope for the variables with windows NT shell
